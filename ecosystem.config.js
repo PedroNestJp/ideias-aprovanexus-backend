57b32aa5121c526aaea546c39abcf8991b1e032f
nexus <grupoeducacionalnexus>
2025-05-06 11:48:39 -0300

    fix: updated project version in ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "inova-v1.1",
+      name: "inova-v1.2",
       script: "dist/main.js", // ou "src/main.ts" se usar ts-node
       watch: true,
       interpreter: "node", // ou "ts-node"
