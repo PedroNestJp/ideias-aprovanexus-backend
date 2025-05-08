5c7643aabf171e108ec6247654eb12fc9cd6020f
nexus <grupoeducacionalnexus>
2025-05-08 08:08:08 -0300

    fix: Remove unused function in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,14 +11,8 @@ async function bootstrap() {
       credentials: true,
     });
     app.setGlobalPrefix('/');
-    app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-      prefix: '/uploads/',
-    });
   } else {
     app.setGlobalPrefix('api');
-    app.useStaticAssets(join('/var/www/inova-uploads'), {
-      prefix: '/uploads/',
-    });
   }
   await app.listen(3004);
 }
